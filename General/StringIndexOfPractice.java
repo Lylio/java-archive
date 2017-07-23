026da5c970c89c3c14513877c343e4c2103a3dfc
Lyle Christine <cxz@gmx.com>
2017-07-23 09:31:43 +0100

    Practice with indexOf method on Strings

--- General/StringIndexOfPractice.java
+++ General/StringIndexOfPractice.java
@@ -9,14 +9,19 @@ public class StringIndexOfPractice {
 	public static void main(String[] args) {
 		
 		String sentence = "Picture yourself on a boat on a river.";
-		int indexLocation = sentence.indexOf("boat");
-		System.out.println(indexLocation); //22 expected
+		int indexLocation = sentence.indexOf("on");
+		System.out.println(indexLocation); //17 expected
 		
 		int charLocation = sentence.indexOf('a');
 		System.out.println(charLocation); //20 expected
 		
 		int charLocation2 = sentence.indexOf('a', 22);
 		System.out.println(charLocation2); //24 expected as search starts from index 22
+		
+		int indexLocation2 = sentence.lastIndexOf("on");
+		System.out.println(indexLocation2); /*lastIndexOf searches right to left
+											27 expected as the second "on" will be
+											found first*/
 		
 		
 		//additional charAt practice
