16e2b3e5be5dc607a69483c23c149c283f3626b9
Lyle Christine <cxz@gmx.com>
2017-07-29 10:11:35 +0100

    Class practice with constructor, get and set methods.

---
+++ General/Buttons.java
@@ -0,0 +1,27 @@
+public class Buttons {
+
+	private int numberOfButtons;
+
+
+	//constructor
+	public Buttons(int numberOfButtons) {
+		this.numberOfButtons = numberOfButtons;
+	}
+
+	//get Method
+	public int getNumberOfButtons() {
+
+		return numberOfButtons;
+
+	}
+
+
+	//set Method
+	public void setNumberOfButtons(int newButtonAmount) {
+
+		numberOfButtons = newButtonAmount;
+
+	}
+
+}
+
