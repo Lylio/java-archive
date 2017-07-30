cc2603e1c1a82548af090590994cd68682118323
Lyle Christine <cxz@gmx.com>
2017-07-30 14:13:33 +0100

    Interface practice

--- InterfacePractice/InterfacePractice.java
+++ InterfacePractice/InterfacePractice.java
@@ -1,57 +1,19 @@
 
-public class InterfacePractice implements GreetingsInterface, FarewellInterface {
+public class InterfacePractice {
 
 	public static void main(String[] args) {
 		
-	InterfacePractice test = new InterfacePractice();
-	test.hello();
-	
-	InterfacePractice test2 = new InterfacePractice();
-	test2.gesture();
-	
-	test.smile();
+		MainInterface myInterface = new MainInterface();
+		
+		myInterface.hello();
+		
+		myInterface.gesture();
+		
+		myInterface.smile();
 	
 
-	}
-	
-	@Override
-	public void hello() {
-		System.out.println("Hello!");
 	}
 	
-	@Override
-	public void handShake() {
-		System.out.println("*SHAKES HAND*");
-	}
-
-	@Override
-	public void smile() {
-		System.out.println(":)");
-		
-	}
-
-	@Override
-	public void chitChat() {
-		System.out.println("Nice weather for ducks!");
-		
-	}
-
-	@Override
-	public void goodbye() {
-		System.out.println("Cheerie bye!");
-		
-	}
-
-	@Override
-	public void gesture() {
-		System.out.println("*WAVES!*");
-		
-	}
-
-	@Override
-	public void tears() {
-		System.out.println("Sob! I'll miss you!");
-		
-	}
+	
 
 }
