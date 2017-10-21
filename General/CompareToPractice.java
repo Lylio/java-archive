dffa61550e1370b12b2a299aa9604f4f2aae6e3b
Lyle Christine <cxz@gmx.com>
2017-10-21 19:05:32 +0100

    Practice with compareTo method

---
+++ General/CompareToPractice.java
@@ -0,0 +1,14 @@
+
+public class CompareToPractice {
+
+	public static void main(String[] args) {
+		
+		String word1 = "Apples";
+		String word2 = "Grapes";
+		
+		System.out.println(word1.compareTo(word2)); //-6 expected: 'A' is six along in 
+													//the alphabet from 'G' 
+
+	}
+
+}
