1ba16543a9bc92b7a1145763e8e01306720e4e41
lylio <cxz@gmx.com>
2019-07-13 12:59:11 +0100

    Unary operator

---
+++ UnaryPractice.java
@@ -0,0 +1,14 @@
+public class UnaryPractice {
+
+    public static void main(String[] args) {
+        proceed();
+    }
+
+    public static void proceed() {
+
+        //Unary operator (~) outputs 1 less than the
+        // negative of the argument
+        System.out.println("Unary (~) of 55 = " + ~ 55); //-54
+    }
+}
+
