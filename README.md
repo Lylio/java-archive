741ea22f4e8f6b82dd18b57c4f1ad1ce499054a6
lylio <cxz@gmx.com>
2019-08-30 09:04:56 +0100

    update

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
 # General Java Practice
 Various Java programs demonstrating a range of techniques, from beginner to advanced.
 
-![Java Logo](https://github.com/Lylio/images/blob/master/java-logo-smaller.jpg)
+![Java Logo](java-logo.png)
--- com/lylechristine/jmx/JMXDemoLancher.java
+++ com/lylechristine/jmx/JMXDemoLancher.java
@@ -1,5 +1,29 @@
 package com.lylechristine.jmx;
 
+import javax.management.*;
+import java.lang.management.ManagementFactory;
+
 public class JMXDemoLancher {
+
+    public static void main(String[] args) {
+
+        ObjectName objectName = null;
+        try {
+            objectName = new ObjectName("com.lylechristine.jmx:type=basic,name=game");
+        } catch (MalformedObjectNameException e) {
+            e.printStackTrace();
+        }
+        MBeanServer server = ManagementFactory.getPlatformMBeanServer();
+        Game gameObj = new Game();
+        try {
+            server.registerMBean(gameObj, objectName);
+        } catch (InstanceAlreadyExistsException | MBeanRegistrationException | NotCompliantMBeanException e) {
+            e.printStackTrace();
+        }
+
+        while (true) {
+            // to ensure application does not terminate
+        }
+    }
 }
 
